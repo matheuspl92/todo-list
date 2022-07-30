c78cff1614449e452d45021ebecd7cd9c016d286
Matheus <matheuspl92@hotmail.com>
2022-07-29 21:16:24 -0300

    Setup basic webpack development environment

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Todo Now',
+          /*favicon: '',*/
+        }),
+      ],
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
+            type: 'asset/resource',
+          },
+        ],
+      },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+};
