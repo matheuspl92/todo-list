8e6ec10d4f71c11893fe6c8aad6d03021fa1e72e
Matheus <matheuspl92@hotmail.com>
2022-08-01 19:57:22 -0300

    Create todo factory module

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,3 +1,21 @@
-const createTodo = (name, description, dueDate, priority) => {
-    return {name, description, dueDate, priority}
+const Todo = (name, description, dueDate, priority) => {
+    let todoName = name;
+    let todoDescription = description;
+    let todoDueDate = dueDate;
+    let todoPriority = priority;
+
+    return {
+        getName: () => {todoName},
+        getDescription: () => {todoDescription},
+        getDueDate: () => {todoDueDate},
+        getPriority: () => {todoPriority},
+
+        editName: (newName) => {todoName = newName},
+        editDescription: (newDescription) => {todoDescription = newDescription},
+        editDueDate: (newDueDate) => {todoDueDate = newDueDate},
+        editPriority: (newPriority) => {todoPriority = newPriority},
+    
+    }
 }
+
+export default Todo;
