0a04e0ea524d50ef34e3007900cb57e272002069
Matheus <matheuspl92@hotmail.com>
2022-08-01 19:48:25 -0300

    Create project factory module

---
+++ src/modules/project.js
@@ -0,0 +1,20 @@
+const Project = (name) => {
+    let projectName = name;
+    const todoArray = [];
+
+    return {
+        getProjectName: () => {return projectName},
+
+        addTodo: (todo) => {todoArray.push(todo)},
+
+        getTodoArray: () => {return todoArray.slice()},
+
+        removeTodo: (todoIndex) => {todoArray.splice(todoIndex, 1)},
+
+        editTodo: (todoIndex, todo) => {todoArray[todoIndex] = todo},
+
+
+    }
+}
+
+export default Project;
